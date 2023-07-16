57b5c58e6657183b2eb323eb08c5fea92817ca27
Drew Neece <dneece5@aggienetwork.com>
2023-07-16 14:16:49 -0500

    complete orderID challenge

--- Program.cs
+++ Program.cs
@@ -6,9 +6,29 @@
 
 // Console.WriteLine($"First: {fraudOrderIDs[0]}\nSecond: {fraudOrderIDs[1]}\nThird: {fraudOrderIDs[2]}");
 
-string[] names = { "Rowena", "Robin", "Bao" };
+// string[] names = { "Rowena", "Robin", "Bao" };
 
-foreach (string name in names)
-{
-  Console.WriteLine(name);
+// foreach (string name in names)
+// {
+//   Console.WriteLine(name);
+// }
+
+// int[] inventory = { 200, 450, 700, 175, 250 };
+// int sum = 0;
+// int bin = 0;
+
+// foreach (int item in inventory) {
+// sum += item;
+// bin++;
+// Console.WriteLine($"Bin {bin} = {item} items (Running total: {sum})");
+// }
+
+// Console.WriteLine($"We have {sum} items in inventory.");
+
+string[] orderIDs = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+
+foreach (string id in orderIDs) {
+  if (id.StartsWith("B")) {
+    Console.WriteLine(id);
+  }
 }

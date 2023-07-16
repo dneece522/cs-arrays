fe835e2212caddefc136490ce9ca48e51a12f960
Drew Neece <dneece5@aggienetwork.com>
2023-07-16 14:06:17 -0500

    first array and foreach

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// string[] fraudOrderIDs = { "A123", "B456", "C789" };
+
+// fraudOrderIDs[0] = "A123";
+// fraudOrderIDs[1] = "B456";
+// fraudOrderIDs[2] = "C789";
+
+// Console.WriteLine($"First: {fraudOrderIDs[0]}\nSecond: {fraudOrderIDs[1]}\nThird: {fraudOrderIDs[2]}");
+
+string[] names = { "Rowena", "Robin", "Bao" };
+
+foreach (string name in names)
+{
+  Console.WriteLine(name);
+}
